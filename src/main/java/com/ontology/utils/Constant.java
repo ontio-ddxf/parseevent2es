2a8d3c14110f8520dcf2b9ec3730310c1aa99c79
leej1012 <leej1012@aliyun.com>
2019-10-17 16:15:16 +0800

    增加常量类

---
+++ src/main/java/com/ontology/utils/Constant.java
@@ -0,0 +1,27 @@
+package com.ontology.utils;
+
+/**
+ * 常量
+ */
+public class Constant {
+
+    /**
+     * ES索引和类型
+     */
+    public static final String ES_INDEX_SYNC = "sync_index";
+    public static final String ES_TYPE_SYNC = "blockHeight";
+
+    /**
+     * KAFKA topic
+     */
+    public static final String KAFKA_TOPIC_EVENT = "topic-block-event";
+
+    public static final String KAFKA_TOPIC_SOURCING = "topic-block-event";
+
+    public static final String KAFKA_TOPIC_MP = "topic-block-event";
+
+    /**
+     * KAFKA group
+     */
+    public static final String KAFKA_GROUP_EVENT = "topic-group-event";
+}
